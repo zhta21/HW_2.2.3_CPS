f37e5e3aaae4fedf4964c17f2503efc6f4b258a4
Taras Zhurenko <zhta21@yandex.ru>
2021-12-14 02:25:00 +0300

    Create CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,3 +1,9 @@
 public class CreditPaymentService {
-
+    double calculate(int creditAmount,int creditPeriod, double creditRate) {
+        double monthlyCreditRate = creditRate / 12 / 100;
+        int monthlyCreditPeriod = creditPeriod * 12;
+        double creditRatio = monthlyCreditRate + (monthlyCreditRate / (Math.pow((1 + monthlyCreditRate),monthlyCreditPeriod) - 1));
+        int monthlyPayment = (int) (creditRatio * creditAmount);
+        return monthlyPayment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
 
+        int monthlyPaymentOneYear = (int) service.calculate(1_000_000,1,9.99);
+        System.out.println(monthlyPaymentOneYear);
+
+        int monthlyPaymentTwoYear = (int) service.calculate(1_000_000,2,9.99);
+        System.out.println(monthlyPaymentTwoYear);
+
+        int monthlyPaymentThreeYear = (int) service.calculate(1_000_000,3,9.99);
+        System.out.println(monthlyPaymentThreeYear);
     }
 }
